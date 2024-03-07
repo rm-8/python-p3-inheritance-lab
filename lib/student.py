3df1156d69e3628fc7c440fb172f7df5362b7622
rm-8 <mohamedridhwan8@gmail.com>
2024-03-07 10:36:52 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,20 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
+
+mike = Student("Mike", "Tyson")
+print(mike.learn("ugali"))
+print(mike.learn("ugali2"))
+
+
+
+    
+    
+    
